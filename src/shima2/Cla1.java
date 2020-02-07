2f59b18229eecb7078a713aa574e2777f741bd85
shima <shima.osamah@yahoo.com>
2020-02-07 11:45:41 +0200

    hola

--- src/shima2/Cla1.java
+++ src/shima2/Cla1.java
@@ -2,12 +2,20 @@ package shima2;
 
 public class Cla1 {
 	public static int add (int x ,int y) {
+		System.out.print(false);
+		
 		return x+y;
+		
+		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
      int z = add(2,40);
+     
+     
+     System.out.print(false);
 	}
 
 }
